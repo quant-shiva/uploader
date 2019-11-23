0bd2f809717bd721a9bf6f5f02eef36e8b615569
SHIVAM KR TIWARI <shivamtind@gmail.com>
2019-11-23 10:51:25 +0530

    removing seperate congig file for cloudinary

--- config/cloudinary.js
+++
@@ -1,7 +0,0 @@
-var cloudinary = require("cloudinary");
-
-module.exports = cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
-});
